864fb5477c24303043b0ed6f6901cf355ebf10f1
lucas <lucas@ak.com>
2020-12-05 04:31:37 +0800

    add alibaba-cloud-sdk-go

---
+++ src/github.com/aliyun/alibaba-cloud-sdk-go/sdk/logger_test.go
@@ -0,0 +1,54 @@
+package sdk
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_OpenLogger(t *testing.T) {
+	client, err := NewClientWithAccessKey("regionid", "acesskeyid", "accesskeysecret")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, true, client.isRunning)
+
+	client.OpenLogger()
+	assert.Equal(t, true, client.logger.isOpen)
+}
+
+func Test_SetTemplate(t *testing.T) {
+	client, err := NewClientWithAccessKey("regionid", "acesskeyid", "accesskeysecret")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, true, client.isRunning)
+
+	template := "{time}"
+	client.SetTemplate(template)
+	assert.Equal(t, "{time}", client.logger.formatTemplate)
+}
+
+func Test_GetTemplate(t *testing.T) {
+	client, err := NewClientWithAccessKey("regionid", "acesskeyid", "accesskeysecret")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, true, client.isRunning)
+
+	assert.Equal(t, defaultLoggerTemplate, client.GetTemplate())
+}
+
+func Test_GetLoggerMsg(t *testing.T) {
+	client, err := NewClientWithAccessKey("regionid", "acesskeyid", "accesskeysecret")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, true, client.isRunning)
+
+	assert.Equal(t, "", client.GetLoggerMsg())
+}
+
+func Test_TransToString(t *testing.T) {
+	x := map[string]interface{}{
+		"foo": make(chan int),
+	}
+	str := TransToString(x)
+	assert.Equal(t, str, "")
+}
